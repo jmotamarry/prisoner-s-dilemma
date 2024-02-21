8cf711060f4e4ce6ba62326331dad97e6b885e95
Jay Motamarry <79808791+jmotamarry@users.noreply.github.com>
2024-02-20 23:39:08 -0800

    semi-working rl IPD

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import numpy as np
 import tensorflow as tf
+import random
+import matplotlib.pyplot as plt
 
 class DQNAgent:
     def __init__(self, state_size, action_size, memory_size=1000, gamma=0.95, epsilon=1.0, epsilon_decay=0.995, epsilon_min=0.01):
@@ -23,6 +25,8 @@ class DQNAgent:
         return model
 
     def remember(self, state, action, reward, next_state, done):
+        state = np.ravel(state)
+        next_state = np.ravel(next_state)
         self.memory.append((state, action, reward, next_state, done))
         if len(self.memory) > self.memory_size:
             self.memory.pop(0)
@@ -36,21 +40,34 @@ class DQNAgent:
     def replay(self, batch_size):
         if len(self.memory) < batch_size:
             return
-        minibatch = np.random.choice(self.memory, batch_size, replace=False)
-        for state, action, reward, next_state, done in minibatch:
-            target = reward
-            if not done:
-                target = reward + self.gamma * np.amax(self.model.predict(next_state)[0])
-            target_f = self.model.predict(state)
-            target_f[0][action] = target
-            self.model.fit(state, target_f, epochs=1, verbose=0)
+
+        minibatch = random.sample(self.memory, batch_size)
+
+        states = np.array([np.ravel(np.array(sample[0])) for sample in minibatch])
+        actions = np.array([sample[1] for sample in minibatch])
+        rewards = np.array([sample[2] for sample in minibatch])
+        next_states = np.array([np.ravel(np.array(sample[3])) for sample in minibatch])
+        dones = np.array([sample[4] for sample in minibatch])
+
+        targets = self.model.predict(states)
+        next_q_values = self.model.predict(next_states)
+
+        for i in range(batch_size):
+            target = rewards[i]
+            if not dones[i]:
+                target += self.gamma * np.amax(next_q_values[i])
+
+            targets[i, actions[i]] = target
+
+        self.model.fit(states, targets, epochs=1, verbose=2)
+
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
 
 def play_game(agent1, agent2, rounds, rewards_matrix):
     total_rewards = {0: 0, 1: 0}
 
-    for _ in range(rounds):
+    for episode in range(rounds):
         state = np.zeros((1, 2))
         action1 = agent1.act(state)
         action2 = agent2.act(state)
@@ -66,11 +83,32 @@ def play_game(agent1, agent2, rounds, rewards_matrix):
     return total_rewards
 
 def train_agents(agent1, agent2, rounds, rewards_matrix):
-    for _ in range(rounds):
-        play_game(agent1, agent2, 1, rewards_matrix)
+    rewards_history = []
+
+    for episode in range(rounds):
+        total_rewards = play_game(agent1, agent2, 1, rewards_matrix)
+        print(f"Episode {episode + 1}, Agent 1 Total Rewards: {total_rewards[0]}, Agent 2 Total Rewards: {total_rewards[1]}")
+
         agent1.replay(32)
-        play_game(agent2, agent1, 1, rewards_matrix)
+        total_rewards = play_game(agent2, agent1, 1, rewards_matrix)
+        print(f"Episode {episode + 1}, Agent 2 Total Rewards: {total_rewards[1]}")
         agent2.replay(32)
+
+        rewards_history.append(total_rewards)
+
+    plot_rewards(rewards_history)
+
+def plot_rewards(rewards_history):
+    agent1_rewards = [episode[0] for episode in rewards_history]
+    agent2_rewards = [episode[1] for episode in rewards_history]
+
+    plt.plot(agent1_rewards, label='Agent 1')
+    plt.plot(agent2_rewards, label='Agent 2')
+    plt.xlabel('Episode')
+    plt.ylabel('Total Rewards')
+    plt.title('Training Progress')
+    plt.legend()
+    plt.show()
 
 def main():
     state_size = 2
@@ -78,12 +116,12 @@ def main():
     agent1 = DQNAgent(state_size, action_size)
     agent2 = DQNAgent(state_size, action_size)
 
-    rounds = 1000
-    rewards_matrix = np.array([[3, 0], [5, 1]])  # Adjust rewards as needed
+    rounds = 50
+    rewards_matrix = np.array([[1, 5], [0, 5]])  # Adjust rewards as needed
 
     train_agents(agent1, agent2, rounds, rewards_matrix)
 
-    final_rewards = play_game(agent1, agent2, 100, rewards_matrix)
+    final_rewards = play_game(agent1, agent2, 1, rewards_matrix)
     print("Final Game Results:")
     print(f"Agent 1 Total Rewards: {final_rewards[0]}")
     print(f"Agent 2 Total Rewards: {final_rewards[1]}")
